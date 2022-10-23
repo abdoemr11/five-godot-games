fd47da4222e52830d178d48934df9a051b3fa5ba
abdoemr11 <abdoemr11@gmail.com>
2022-10-23 07:14:13 +0200

    finished the main base level and level 1

---
+++ jungle_jump/levels/Level.gd
@@ -0,0 +1,39 @@
+extends Node2D
+
+var Collectible = preload("res://collectible/Collectible.tscn")
+onready var pickups = $Pickups
+
+#Score variables
+signal score_changed
+var score
+
+func _ready():
+	score = 0
+	emit_signal("score_changed", score)
+	pickups.hide()
+	$Player.start($PlayerSpawn.position)
+	set_camera_limits()
+	
+func set_camera_limits():
+	var map_size = $World.get_used_rect()
+	var cell_size = $World.cell_size
+	$Player/Camera2D.limit_left = (map_size.position.x - 5) * cell_size.x
+	$Player/Camera2D.limit_right = (map_size.end.x + 5) * cell_size.x
+
+func spawn_pickups():
+	for cell in pickups.get_used_cells():
+		var id = pickups.get_cellv(cell)
+		var type = pickups.tile_set.tile_get_name(id)
+		if type in ['cherry', 'gem']:
+			var c = Collectible.instance()
+			var pos = pickups.map_to_world(cell)
+			c.init(type, pos + pickups.cell_size/2)
+			add_child(c)
+			c.connect('pickup', self, '_on_Collectible_pickup')
+
+func _on_Collectible_pickup():
+	score += 1
+	emit_signal("score_changed", score)
+
+func _on_Player_dead():
+	pass
